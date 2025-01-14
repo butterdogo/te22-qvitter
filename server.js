140d6d243977faf49f679be1809a0e2653710c36
olle <olle.axelsson@elev.ga.ntig.se>
2025-01-14 11:15:13 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+import "dotenv/config"
+import express from "express"
+import nunjucks from "nunjucks"
+
+const app = express()
+const port = 3000
+
+nunjucks.configure("views", {
+    autoescape: true,
+    express: app,
+})
+
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+    res.render("index.njk",{
+        title: "Start",
+        message: "Halloj fisk"
+    })
+
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
